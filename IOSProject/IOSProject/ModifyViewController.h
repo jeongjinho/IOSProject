3252555d1ba171e3fde491136efced5b128a46a9
yangdaesung <yanggogo2333@gmail.com>
2016-11-28 18:07:25 +0900

    modifiyViewController Create

---
+++ IOSProject/IOSProject/ModifyViewController.h
@@ -0,0 +1,13 @@
+//
+//  ModifyViewController.h
+//  IOSProject
+//
+//  Created by Yang on 2016. 11. 28..
+//  Copyright © 2016년 진호놀이터. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ModifyViewController : UIViewController
+
+@end
---
+++ IOSProject/IOSProject/ModifyViewController.m
@@ -0,0 +1,37 @@
+//
+//  ModifyViewController.m
+//  IOSProject
+//
+//  Created by Yang on 2016. 11. 28..
+//  Copyright © 2016년 진호놀이터. All rights reserved.
+//
+
+#import "ModifyViewController.h"
+
+@interface ModifyViewController ()
+
+@end
+
+@implementation ModifyViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
