b3f9adeddb060de72c4e5bedda86c67d072b3322
진호놀이터 <jinhos@jinhonol-iteoui-MacBook-Pro.local>
2016-11-28 15:37:03 +0900

    add loginPage

---
+++ IOSProject/IOSProject/CustomTextField.h
@@ -0,0 +1,13 @@
+//
+//  CustomTextField.h
+//  IOSProject
+//
+//  Created by 진호놀이터 on 2016. 11. 28..
+//  Copyright © 2016년 진호놀이터. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CustomTextField : UITextField
+
+@end
---
+++ IOSProject/IOSProject/CustomTextField.m
@@ -0,0 +1,26 @@
+//
+//  CustomTextField.m
+//  IOSProject
+//
+//  Created by 진호놀이터 on 2016. 11. 28..
+//  Copyright © 2016년 진호놀이터. All rights reserved.
+//
+
+#import "CustomTextField.h"
+
+@implementation CustomTextField
+
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+    CALayer *emailBorder = [CALayer layer];
+    emailBorder.frame = CGRectMake(0.0f, self.frame.size.height - 1, self.frame.size.width, 1.0f);
+    emailBorder.backgroundColor = [UIColor whiteColor].CGColor;
+    [self.layer addSublayer:emailBorder];
+    
+    [[UIColor blueColor] setFill];
+    
+    [self setValue:[UIColor whiteColor] forKeyPath:@"_placeholderLabel.textColor"];
+}
+
+
+@end
