150b09e7cb62eb006f755c10bd4d2df6721d27d2
진호놀이터 <jinhos@jinhonol-iteoui-MacBook-Pro.local>
2016-11-29 17:01:00 +0900

    add aoeck stringlength

--- IOSProject/IOSProject/LoginViewController.m
+++ IOSProject/IOSProject/LoginViewController.m
@@ -8,10 +8,14 @@
 
 #import "LoginViewController.h"
 #import "CustomTextField.h"
+static NSString *const emailText =  @"emailTextField.text";
+static NSString *const passwordText = @"passwordTextField.text";
+
 @interface LoginViewController ()<UITextFieldDelegate>
 @property (weak, nonatomic) IBOutlet CustomTextField *emailTextField;
 @property (weak, nonatomic) IBOutlet CustomTextField *passwordTextField;
-
+@property (weak, nonatomic) IBOutlet UIButton *loginButton;
+@property (strong,nonatomic) UITapGestureRecognizer *windowTap;
 @end
 
 @implementation LoginViewController
@@ -20,14 +24,59 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
      self.navigationController.navigationBar.hidden = YES;
-        
     self.emailTextField.delegate = self;
     self.passwordTextField.delegate = self;
     //유저텍스트필드 히든
     self.passwordTextField.secureTextEntry = YES;
+    
+    //observing
+    [self addObserver:self forKeyPath:emailText options:NSKeyValueObservingOptionInitial | NSKeyValueObservingOptionPrior | NSKeyValueObservingOptionNew | NSKeyValueObservingOptionOld context:nil];
+    
+    [self addObserver:self forKeyPath:passwordText options:NSKeyValueObservingOptionInitial | NSKeyValueObservingOptionPrior | NSKeyValueObservingOptionNew | NSKeyValueObservingOptionOld context:nil];
+    //observing에 따른 로그인 화면변화
+    self.loginButton.userInteractionEnabled =NO;
+    self.loginButton.layer.opacity = 0.1f;
 
+    
+    self.windowTap=[[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(resignKeyboard:)];
+    
+    [self.view addGestureRecognizer:self.windowTap];
 }
 
+-(void)resignKeyboard:(UITapGestureRecognizer*)tap{
+
+    [self.emailTextField resignFirstResponder];
+    [self.passwordTextField resignFirstResponder];
+
+
+}
+#pragma -mark login textField
+-(void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary<NSKeyValueChangeKey,id> *)change context:(void *)context{
+
+
+    if ([keyPath isEqualToString:emailText]) {
+        NSLog(@"The email is changed.");
+        
+    }
+    
+    if ([keyPath isEqualToString:passwordText]) {
+        NSLog(@"The password is changed.");
+        NSLog(@"text : %ld , password :%ld",self.emailTextField.text.length,self.passwordTextField.text.length);
+        
+        if(self.emailTextField.text.length  >=5 && self.passwordTextField.text.length >= 8){
+            self.loginButton.layer.opacity = 1.0f;
+            self.loginButton.userInteractionEnabled = YES;
+            
+        } else{
+            self.loginButton.userInteractionEnabled =NO;
+            self.loginButton.layer.opacity = 0.1f;
+
+        }
+        
+    }
+
+}
+#pragma -mark textField delegate
 -(BOOL)textFieldShouldReturn:(UITextField *)textField{
     //아이디를 쓰고 리턴을 눌렀을때idTextField tag = 0 , passwordTextField tag = 1
     switch (textField.tag) {
