74d2f893b7a7e7e3592bd1ce087ed920567e03c7
진호놀이터 <jinhos@jinhonol-iteoui-MacBook-Pro.local>
2016-12-03 20:09:50 +0900

    using Photos frameWork  create WritePage to select  Group main Image

---
+++ IOSProject/IOSProject/WriteCollectionViewCell.m
@@ -0,0 +1,16 @@
+//
+//  WriteCollectionViewCell.m
+//  IOSProject
+//
+//  Created by 진호놀이터 on 2016. 12. 3..
+//  Copyright © 2016년 진호놀이터. All rights reserved.
+//
+
+#import "WriteCollectionViewCell.h"
+@interface WriteCollectionViewCell()
+
+
+@end
+@implementation WriteCollectionViewCell
+
+@end
--- IOSProject/IOSProject/WriteViewController.m
+++ IOSProject/IOSProject/WriteViewController.m
@@ -7,10 +7,16 @@
 //
 
 #import "WriteViewController.h"
-#import <Photos/PHCollection.h>
 #import <Photos/PHAsset.h>
+#import <Photos/PHCollection.h>
+#import <Photos/PHImageManager.h>
+#import "WriteCollectionViewCell.h"
+
 @interface WriteViewController ()<UICollectionViewDelegate,UICollectionViewDataSource>
 @property (weak, nonatomic) IBOutlet UICollectionView *bottomCollectionView;
+@property (weak, nonatomic) IBOutlet UIImageView *topIamgeView;
+@property (strong,nonatomic) NSMutableArray *loadImageData;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *writeButtonHeightConstraint;
 
 @end
 
@@ -20,37 +26,58 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
 //    self.tabBarController.tabBar.hidden = YES;
+    
+    [self loadImageInDevicePhotoLibray];
+    NSLog(@"self number: %ld",self.loadImageData.count);
+
     self.bottomCollectionView.delegate = self;
     self.bottomCollectionView.dataSource = self;
     
-    //라이브러리
    
+    
+}
 
-    __block NSString *localIdentifier = nil;
-    // 카메라 롤 앨범을 읽어온다.
+- (void)loadImageInDevicePhotoLibray{
+ 
     PHFetchResult *albumList = [PHAssetCollection fetchAssetCollectionsWithType:PHAssetCollectionTypeSmartAlbum
-                                                                               subtype:PHAssetCollectionSubtypeSmartAlbumUserLibrary
-                                                                               options:nil];
+                                                                        subtype:PHAssetCollectionSubtypeSmartAlbumUserLibrary
+                                                                        options:nil];
     
     PHAssetCollection *smartFolderAssetCollection = (PHAssetCollection *)[albumList firstObject];
-//    
-//    // 카메라 롤에 있는 사진을 가져온다.
+    //
+    //    // 카메라 롤에 있는 사진을 가져온다.
     PHFetchResult *assets = [PHAsset fetchAssetsInAssetCollection:smartFolderAssetCollection  options:nil];
-    [assets enumerateObjectsUsingBlock : ^(PHAsset *asset, NSUInteger idx, BOOL *stop) {
-        // PHAsset에서 localIdentifier 프로퍼티를 읽는다.
-        localIdentifier = asset.localIdentifier;
-        *stop = YES;
-    }];
- 
-}
+    PHImageRequestOptions *options = [[PHImageRequestOptions alloc] init];
+    options.networkAccessAllowed = YES;
+    options.synchronous = YES;
+    options.resizeMode = PHImageRequestOptionsResizeModeExact;
+    PHImageManager *photoManager = [PHImageManager defaultManager];
+    __block NSMutableArray  *loadImages = [NSMutableArray array];
+    
+    for (PHAsset *asset in assets) {
+        
+            [photoManager requestImageForAsset:asset targetSize:PHImageManagerMaximumSize contentMode:PHImageContentModeDefault options:options resultHandler:^(UIImage * _Nullable result, NSDictionary * _Nullable info) {
+                
+               
+                [loadImages addObject:result];
+                 
+            }];
+        
+        NSLog(@"로드이미지 카운트 : %ld",loadImages.count);
+    }
+    
+    self.loadImageData = [NSMutableArray arrayWithArray:loadImages];
+    
+    }
+
 #pragma -mark CollectionView delegate Method
 -(NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView{
 
     return 1;
 }
 -(NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section{
-
-    return 40;
+    NSLog(@"갯수 :%ld",self.loadImageData.count);
+    return self.loadImageData.count;
 }
 - (CGFloat)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout minimumInteritemSpacingForSectionAtIndex:(NSInteger)section
 {
@@ -66,20 +93,39 @@
     return UIEdgeInsetsMake(0, 1, 0, 1);
     
 }
+
 - (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath
 {
     
     return CGSizeMake(self.bottomCollectionView.frame.size.width/4-2,self.bottomCollectionView.frame.size.width/4-2);
 }
+
 -(UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath{
 
-    UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"PhotoCell" forIndexPath:indexPath];
+    WriteCollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"PhotoCell" forIndexPath:indexPath];
+    
+    UIImage *loadImage = self.loadImageData[indexPath.row];
+    cell.collectionViewImage.image = loadImage;
+    
+
     return cell;
 
+}
+
+-(void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath{
+    //물어볼것
+   // WriteCollectionViewCell *cell = [collectionView cellForItemAtIndexPath:indexPath];
+    self.topIamgeView.image = self.loadImageData[indexPath.row];
+
+    
 }
 #pragma -mark touch In Side BackButton
 - (IBAction)touchInSideBackTapVC:(id)sender {
     [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (IBAction)touchInSidedWriteButton:(id)sender {
+    NSLog(@"글쓰기 버튼 눌림 ");
 }
 
 - (void)didReceiveMemoryWarning {
